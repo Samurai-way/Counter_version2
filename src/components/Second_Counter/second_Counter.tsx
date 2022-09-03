89dc74f5454c31af6e82845c0a663dd50751eb42
Samurai-way <olegmoishevych@gmail.com>
2022-09-03 22:03:09 +0300

    change counters

--- src/components/Second_Counter/second_Counter.tsx
+++ src/components/Second_Counter/second_Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './second_Counter.module.css';
 import {UniversalButton} from "../universalButton/universalButton";
 
@@ -15,16 +15,29 @@ export const SecondCounter = (props: SecondCounterPropsType) => {
 
     const [none, setNone] = useState(false)
 
+
     const setToggle = () => {
         setNone(!props.toggle)
         props.setToggle(false)
     }
 
+    const onChangeHandlerInput =(e: ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.currentTarget.value)
+    }
+
     return (
         <div style={none ? display : undefined} className={s.wrapper}>
             <div className={s.inputs_wrapper}>
-                <div className={s.max_input}>Max value: <input type={'number'}/></div>
-                <div className={s.start_input}>Start value: <input type={'number'}/></div>
+                <div className={s.max_input}>Max value:
+                    <input
+                        type={'number'}
+                        onChange={onChangeHandlerInput}
+                    />
+                </div>
+                <div className={s.start_input}>Start value:
+                    <input type={'number'}
+                    />
+                </div>
             </div>
             <div className={s.button_wrapper}>
                 <div className={s.set_button}>
